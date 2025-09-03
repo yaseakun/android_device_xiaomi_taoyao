0eff73029e8b644e180a37c8b5055f3856bc6ceb
Yaseakun p <agathagemink2014@gmail.com>
2025-09-03 14:50:14 +0000

    taoyao: Import Fingerprint build

--- lineage_taoyao.mk
+++ lineage_taoyao.mk
@@ -22,3 +22,14 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := 2203129G
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+SystemName := taoyao_global
+SystemDevice := taoyao
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="taoyao_global-user 14 UKQ1.231003.002 V816.0.21.0.ULIMIXM release-keys" \
+    DeviceName=$(SystemDevice) \
+    DeviceProduct=$(SystemName)
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := Xiaomi/taoyao_global/taoyao:14/UKQ1.231003.002/V816.0.21.0.ULIMIXM:user/release-keys
