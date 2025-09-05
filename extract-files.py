979384f4932f4709f98c9998823ed0b027d7a532
Yaseakun p <agathagemink2014@gmail.com>
2025-09-05 09:29:33 +0000

    taoyao: Import proprietary-file from taoyao V816.0.23.0.ULIMIXM

---
+++ extract-files.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixup_remove,
+    lib_fixups,
+    lib_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'hardware/qcom-caf/sm8350',
+    'hardware/xiaomi',
+    'device/xiaomi/taoyao',
+    'vendor/qcom/opensource/display',
+    'vendor/xiaomi/sm8350-common',
+]
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+}
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+    (
+      'vendor.xiaomi.hardware.campostproc@1.0',
+    ): lib_fixup_vendor_suffix,
+}
+
+blob_fixups: blob_fixups_user_type = {
+    ('vendor/etc/camera/pureShot_parameter.xml', 'vendor/etc/camera/pureView_parameter.xml'): blob_fixup()
+        .regex_replace(r'=(\d+)>', r'="\1">'),
+    'vendor/lib64/hw/camera.qcom.so': blob_fixup()
+        .binary_regex_replace(b'\x73\x74\x5F\x6C\x69\x63\x65\x6E\x73\x65\x2E\x6C\x69\x63', b'\x63\x61\x6D\x65\x72\x61\x5F\x63\x6E\x66\x2E\x74\x78\x74')
+        .replace_needed('libmegface.so', 'libmegface_xiaomi.so')
+        .add_needed('libprocessgroup_shim.so'),
+    'vendor/lib64/libMegviiFacepp-0.5.2.so': blob_fixup()
+        .replace_needed('libmegface.so', 'libmegface_xiaomi.so'),
+    'vendor/lib64/hw/camera.xiaomi.so': blob_fixup()
+        .sig_replace('29 07 00 94', '1F 20 03 D5'),
+    'vendor/lib64/hw/com.qti.chi.override.so': blob_fixup()
+        .add_needed('libprocessgroup_shim.so')
+        .binary_regex_replace(b'system/lib64/libion.so', b'vendor/lib64/libion.so'),
+    'vendor/etc/camera/taoyao_motiontuning.xml': blob_fixup()
+        .regex_replace('xml=version', 'xml version'),
+}  # fmt: skip
+
+
+module = ExtractUtilsModule(
+    'taoyao',
+    'xiaomi',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+    add_firmware_proprietary_file=True,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(
+        module, 'sm8350-common', module.vendor
+    )
+    utils.run()
